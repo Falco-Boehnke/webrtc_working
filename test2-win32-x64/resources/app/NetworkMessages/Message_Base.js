7552401520786edb67ae667a7c2374c233e3243b
Falco <falco.boehnke@hs-furtwangen.de>
2019-06-26 12:23:52 +0200

    What

--- test2-win32-x64/resources/app/NetworkMessages/Message_Base.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var MessageType;
-(function (MessageType) {
-    MessageType["LOGIN"] = "login";
-    MessageType["RTC_OFFER"] = "offer";
-    MessageType["RTC_ANSWER"] = "answer";
-    MessageType["RTC_CANDIDATE"] = "candidate";
-})(MessageType = exports.MessageType || (exports.MessageType = {}));
--- test2-win32-x64/resources/app/renderer.ts
+++
@@ -1,5 +0,0 @@
-import { NetworkConnectionManager } from "./NetworkConnectionManager";
-import { UiElementHandler } from "./UiElementHandler";
-
-UiElementHandler.getAllUiElements();
-const test = new NetworkConnectionManager();
