bb2e6eb64691160edc3e0018340d56ad8f987575
Shenanigator <falco.boehnke@gmail.com>
2019-07-03 11:20:17 +0200

    Deleted buuildfolder

--- WORK-win32-x64/resources/app/renderer.ts
+++
@@ -1,6 +0,0 @@
-import { NetworkConnectionManager } from "./NetworkConnectionManager";
-import { UiElementHandler } from "./DataCollectors/UiElementHandler";
-
-UiElementHandler.getAllUiElements();
-console.log(document.getElementById("loginButton"));
-const test = new NetworkConnectionManager();
